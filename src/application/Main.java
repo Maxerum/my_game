3aff740cd26d3fb444c9c6b2b26aff2944623a23
Max Ivashchenko <ivas.maxon2109@gmail.com>
2020-03-08 23:18:22 +0300

    Great_step

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,12 +1,163 @@
 package application;
 	
-import javafx.application.Application;
+/*import javafx.application.Application;
 import javafx.stage.Stage;
 import view.view_controll;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.BorderPane;*/
 
+/*package tutorialfx.tutorial20;*/
 
+import javafx.animation.*;
+import javafx.application.Application;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.KeyCode;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+import javafx.util.Duration;
+
+public class Main extends Application {
+    @Override
+    public void start(Stage primaryStage) {
+        Pane root = new Pane();
+        Image image = new Image(getClass().getResourceAsStream("black_mamb.png"));
+        ImageView img = new ImageView(image);
+        img.setFitHeight(600);
+        img.setFitWidth(900);
+        root.getChildren().add(img);
+
+        
+        
+        Label label = new Label("SNAKE");
+        label.setFont(new Font("Arial", 100));
+        label.setTranslateX(450);
+        label.setTextFill(Color.web("#0076a3"));
+        root.getChildren().add(label);
+        
+        MenuItem newGame = new MenuItem("NEW GAME");
+        MenuItem options = new MenuItem("SETTINGS");
+        MenuItem exitGame = new MenuItem("EXIT");
+        
+        SubMenu mainMenu = new SubMenu(
+                newGame,options,exitGame
+        );
+        
+        MenuItem sound = new MenuItem("KILLERS");
+        MenuItem video = new MenuItem("RECORDS");
+        MenuItem keys = new MenuItem("INFORMATION");
+        MenuItem optionsBack = new MenuItem("BACK");
+        SubMenu optionsMenu = new SubMenu(
+                sound,video,keys,optionsBack
+        );
+        MenuItem NG1 = new MenuItem("ÒÓÐÍÈÐ");
+        MenuItem NG2 = new MenuItem("ÎÄÈÍ ÇÀÅÇÄ");
+        MenuItem NG3 = new MenuItem("2 ÈÃÐÎÊÀ");
+        MenuItem NG4 = new MenuItem("ÍÀÇÀÄ");
+        SubMenu newGameMenu = new SubMenu(
+                NG1,NG2,NG3,NG4
+        );
+        MenuBox menuBox = new MenuBox(mainMenu);
+
+        newGame.setOnMouseClicked(event->menuBox.setSubMenu(newGameMenu));
+        options.setOnMouseClicked(event->menuBox.setSubMenu(optionsMenu));
+        exitGame.setOnMouseClicked(event-> System.exit(0));
+        optionsBack.setOnMouseClicked(event->menuBox.setSubMenu(mainMenu));
+        NG4.setOnMouseClicked(event-> menuBox.setSubMenu(mainMenu));
+        root.getChildren().addAll(menuBox);
+
+        Scene scene = new Scene(root,900,600);
+        /*scene.setOnKeyPressed(event -> {
+            if (event.getCode() == KeyCode.ESCAPE) {
+               
+                if (!menuBox.isVisible()) {
+                  
+                }
+                else{
+                    ft.setFromValue(1);
+                    ft.setToValue(0);
+                    ft.setOnFinished(evt ->  menuBox.setVisible(false));
+                    ft.play();
+
+                }
+            }
+        }); */
+        FadeTransition ft = new FadeTransition(Duration.seconds(1),menuBox);
+        ft.setFromValue(0);
+                    ft.setToValue(1);
+                    ft.play();
+                    menuBox.setVisible(true);
+        primaryStage.setResizable(false);
+        primaryStage.setTitle("Pause");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+        
+       
+   }
+    private static class MenuItem extends StackPane{
+        public  MenuItem(String name){
+            Rectangle bg = new Rectangle(200,20,Color.BLUE);
+           // bg.setOpacity(0.5);
+
+            Text text = new Text(name);
+            text.setFill(Color.WHITE);
+            text.setFont(Font.font("Arial",FontWeight.BOLD,14));
+
+            setAlignment(Pos.CENTER);
+            getChildren().addAll(bg,text);
+            FillTransition st = new FillTransition(Duration.seconds(0.5),bg);
+            setOnMouseEntered(event -> {
+                st.setFromValue(Color.DARKGRAY);
+                st.setToValue(Color.DARKGOLDENROD);
+                st.setCycleCount(Animation.INDEFINITE);
+                st.setAutoReverse(true);
+                st.play();
+            });
+            setOnMouseExited(event -> {
+                st.stop();
+                bg.setFill(Color.BLUE);
+            });
+        }
+    }
+    private static class MenuBox extends Pane{
+        static SubMenu subMenu;
+        public MenuBox(SubMenu subMenu){
+            MenuBox.subMenu = subMenu;
+
+            setVisible(false);
+            //Rectangle bg = new Rectangle(900,600,Color.LIGHTBLUE);
+           // bg.setOpacity(0.4);
+            getChildren().addAll(subMenu);
+        }
+        public void setSubMenu(SubMenu subMenu){
+            getChildren().remove(MenuBox.subMenu);
+            MenuBox.subMenu = subMenu;
+            getChildren().add(MenuBox.subMenu);
+        }
+    }
+
+   private static class SubMenu extends VBox{
+        public SubMenu(MenuItem...items){
+            setSpacing(15);
+            setTranslateY(100);
+            setTranslateX(50);
+            for(MenuItem item : items){
+                getChildren().addAll(item);
+            }
+        }
+    }
+}
+/*
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
@@ -23,4 +174,4 @@ public class Main extends Application {
 	public static void main(String[] args) {
 		launch(args);
 	}
-}
+}*/
--- src/view/view_controll.java
+++ src/view/view_controll.java
@@ -1,7 +1,10 @@
-package view;
+/*package view;
 
+import javafx.event.Event;
+import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
@@ -13,7 +16,7 @@ public class view_controll {
 	
 	public view_controll() {
 		mainPane=new AnchorPane();
-		mainScene=new Scene(mainPane,800,700);
+		mainScene=new Scene(mainPane,800,600);
 		mainStage =new Stage();
 		mainStage.setScene(mainScene);
 	}
@@ -23,11 +26,42 @@ public class view_controll {
 	}
 	
 	public void createMenuButtons() {
-		Button button = new Button();
-		mainPane.getChildren().add(button);
+		Button[] buttons;
+		buttons=new Button[3];
+		for(int i=0;i<1;i++) {
+			buttons[i] = new Button();
+			buttons[i].setLayoutX(200);
+			buttons[i].setLayoutY(200);
+			buttons[i].setStyle("-fx-background-color:yellow");
+			/*buttons[i].setOnMousePressed(new EventHandler<MouseEvent> (){
+				@Override
+				public void handle(MouseEvent event) {
+						AnchorPane myPane = new  AnchorPane();
+						Scene myScene=new Scene(myPane,800,600);
+						Stage mystage= new Stage();
+						mystage.setScene(myScene);
+				}
+			});*/
+					
+				//}
+				/*mainPane.getChildren().add(buttons[0]);
+			};*/
+			
+
+		/*buttons[0].setOnMouseEntered(new EventHandler<Event>() {
+			@Override
+			public void handle(Event event)
+			{
+				System.out.println("You are pidor");
+			}
+		}*/
+		//);
+/*}
+	
 		
-	}
 	
 	
 	
-}
+	
+	
+}*/
